30844635a8e3760a65b675508063ceb79c389c47
Rahulblogging <rahulprajapatiblogging@gmail.com>
2026-05-12 00:53:04 +0530

    Initial commit

--- app.py
+++ app.py
@@ -121,7 +121,7 @@ def decrypt_file():
 # RUN APP
 if __name__ == '__main__':
 
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
 
     app.run(
         host='0.0.0.0',
